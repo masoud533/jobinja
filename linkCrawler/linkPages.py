fb59ef906018066fc4590ad95d7157cb17570253
masoud533 <mr.masoud_mapar@outlook.com>
2023-08-12 12:43:14 +0330

    completed linkPages.py related linkCrawler

--- linkCrawler/linkPages.py
+++ linkCrawler/linkPages.py
@@ -3,8 +3,16 @@ from bs4 import BeautifulSoup
 from http.client import responses
 
 def pages(url: str, selector: str, selector_type: str):
-    page = responses.get(url)
-    if responses[page.status_code] == 'ok':
-        pass
+    # selector and selector_type will be added soon
+    page = requests.get(url)
+    if responses[page.status_code] == 'OK':
+        soup = BeautifulSoup(page.content, 'html.parser')
+        links = soup.find(class_='paginator').find_all('a')
+        print(links)
+        links = [x.get('href') for x in links]
+        print(links)
     else:
         raise Exception(f"oos, we have an error: {responses[page.status_code]}")
+    
+
+
