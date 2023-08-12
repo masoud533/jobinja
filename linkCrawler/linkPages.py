62c09885059fd62b1edac22d17b67f62f1e466dc
masoud533 <mr.masoud_mapar@outlook.com>
2023-08-12 12:53:54 +0330

    Make corrections on linkPages.py related linkCrawler

--- linkCrawler/linkPages.py
+++ linkCrawler/linkPages.py
@@ -2,17 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from http.client import responses
 
-def pages(url: str, selector: str, selector_type: str):
+def pages(url: str, selector: str, selector_type: str) -> list:
     # selector and selector_type will be added soon
     page = requests.get(url)
     if responses[page.status_code] == 'OK':
         soup = BeautifulSoup(page.content, 'html.parser')
-        links = soup.find(class_='paginator').find_all('a')
-        print(links)
+        try:
+            links = soup.find(class_='paginator').find_all('a')
+        except:
+            raise Exception("something is wrong!!!")
         links = [x.get('href') for x in links]
-        print(links)
     else:
         raise Exception(f"oos, we have an error: {responses[page.status_code]}")
     
-
-
