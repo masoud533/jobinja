c25662f2bf09c267f2fb132fc6663a6116f3ea94
masoud533 <mr.masoud_mapar@outlook.com>
2023-08-12 13:42:20 +0330

    work on ads function related adsLink.py

--- linkCrawler/adsLink.py
+++ linkCrawler/adsLink.py
@@ -5,4 +5,16 @@ from http.client import responses
 
 def ads(url, selector, selector_type):
     # selector and selector_type will be added soon
-    pass
+    page = requests.get(url)
+    if responses[page.status_code] == 'OK':
+        dic = {}
+        soup = BeautifulSoup(page.content, 'html.parser')
+        links = soup.find_all(class_='c-jobListView__titleLink')
+        for link in links:
+            dic[(link.text).replace(" ", "").replace('\n', '')] = link.get('href')
+        print(dic)
+    else:
+        raise Exception(f"oos, we have an error: {responses[page.status_code]}")
+    
+
+ads('https://jobinja.ir/jobs?&filters%5Bjob_categories%5D%5B%5D=&filters%5Bkeywords%5D%5B0%5D=data&filters%5Blocations%5D%5B%5D=&preferred_before=1691678646&sort_by=relevance_desc', '', '')
