23f16b088896859208facfc36e231b320cc45a7c
masoud533 <mr.masoud_mapar@outlook.com>
2023-08-12 12:19:01 +0330

    hadel errors related linkPages

--- linkCrawler/linkPages.py
+++ linkCrawler/linkPages.py
@@ -3,4 +3,8 @@ from bs4 import BeautifulSoup
 from http.client import responses
 
 def pages(url: str, selector: str, selector_type: str):
-    pass
+    page = responses.get(url)
+    if responses[page.status_code] == 'ok':
+        pass
+    else:
+        raise Exception(f"oos, we have an error: {responses[page.status_code]}")
