71924baad03820ab64d79e63269e3d0ddd4a86c3
masoud533 <mr.masoud_mapar@outlook.com>
2023-08-12 13:11:21 +0330

    add ads func related adsLink.py

---
+++ linkCrawler/adsLink.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+from http.client import responses
+
+
+def ads(url, selector, selector_type):
+    # selector and selector_type will be added soon
+    pass
